eaf7c6810636bc6a9428430081ce796bc6aa1188
jaredzystro <jared@seedalliance.org>
2019-11-13 13:46:17 -0800

    Update api.R

--- plumber/api.R
+++ plumber/api.R
@@ -1,5 +1,14 @@
 library(plumber)
 library(jsonlite)
+library(lme4)
+
+echoIt <- function (input_matrix) {
+  
+    output_matrix <- input_matrix
+  
+    return (output_matrix)
+  
+  }
 
 doubleIt <- function (input_matrix) {
   
@@ -7,6 +16,18 @@ doubleIt <- function (input_matrix) {
   
     return (output_matrix)
   
+  }
+
+
+AnovaIt <- function (input_matrix) {
+  
+#  data <- as.data.frame(input_matrix)
+#  colnames(data) <- data[1, ] # first row is header
+#  data <- data[-1, ] 
+  
+  output_matrix <- input_matrix
+  
+  return (output_matrix)
   }
 
 #' Double the numbers that were sent in
@@ -18,6 +39,20 @@ function(body){
   body_matrix <- fromJSON(body)
   
   output_matrix <- doubleIt(body_matrix)
+  
+  toJSON(output_matrix)
+  
+}
+
+#' ANOVA
+#' @param body The numbers to add
+#' @post /AnovaIt
+#' @json
+function(body){
+  
+  body_matrix <- fromJSON(body)
+  
+  output_matrix <- AnovaIt(body_matrix)
   
   toJSON(output_matrix)
   
