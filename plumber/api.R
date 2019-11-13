ab68c6103feebd3fa3dfe60880ace5ceec1d5904
jaredzystro <jared@seedalliance.org>
2019-11-13 12:19:58 -0800

    Update api.R

--- plumber/api.R
+++ plumber/api.R
@@ -1,48 +1,24 @@
 library(plumber)
 library(jsonlite)
 
-#' @get /hello
-#' @html
-function(){
-  "50,50,25,25"
-}
-
-#' Echo the parameter that was sent in
-#' @param msg The message to echo back.
-#' @get /echo
-function(msg=""){
-  list(msg = paste0("The message is: '", msg, "'"))
-}
-
-#' Plot out data from the iris dataset
-#' @param spec If provided, filter the data to only this species (e.g. 'setosa')
-#' @get /plot
-#' @png
-function(spec){
-  myData <- iris
-  title <- "All Species"
-
-  # Filter if the species was specified
-  if (!missing(spec)){
-    title <- paste0("Only the '", spec, "' Species")
-    myData <- subset(iris, Species == spec)
+doubleIt <- function (input_matrix) {
+  
+    output_matrix <- input_matrix * 2
+  
+    return (output_matrix)
+  
   }
 
-  plot(myData$Sepal.Length, myData$Petal.Length,
-       main=title, xlab="Sepal Length", ylab="Petal Length")
-}
-
-#' Add the numbers that were sent in
+#' Double the numbers that were sent in
 #' @param body The numbers to add
-#' @post /sumall
+#' @post /doubleIt
 #' @json
-function(body="[[1,3],[3,4]]"){
+function(){
   
-  my_matrix <- fromJSON(body)
-  # my_df <- as.data.frame(my_matrix)
+  body_matrix <- fromJSON(body)
   
-  my_matrix <- my_matrix*2
+  output_matrix <- doubleIt(body_matrix)
   
-  toJSON(my_matrix)
+  toJSON(output_matrix)
   
 }
