ff1d66acd6ad0b0aecb122e12140e60fb8d686ac
jaredzystro <jared@seedalliance.org>
2019-11-14 10:24:50 -0800

    Update api.R

--- plumber/api.R
+++ plumber/api.R
@@ -1,5 +1,6 @@
 library(plumber)
 library(jsonlite)
+library(lme4)
 
 echoIt <- function (input_matrix) {
   
@@ -17,6 +18,24 @@ doubleIt <- function (input_matrix) {
   
   }
 
+AnovaIt <- function (input_matrix) {
+  
+  data_set <- data.frame(input_matrix[-1, ], stringsAsFactors=FALSE)
+  
+  colnames(data_set) <- input_matrix[1,] # the first row will be the header
+
+  data_set$Entry <- as.factor(data_set$Entry)
+  data_set$Rep <- as.factor(data_set$Rep)
+  data_set[, !names(data_set) %in% c("Entry", "Rep")]  <- as.numeric(data_set[, !names(data_set) %in% c("Entry", "Rep")])
+  
+  l_model <- lmer(Height ~ Entry + (1|Rep), data=data_set)
+  anova_data <- anova(l_model)
+  output_df <- as.data.frame(anova_data)
+  output_matrix <- as.matrix(output_df)
+  dimnames(output_matrix) <- list(rownames(output_df),colnames(output_df))
+  
+  return (output_matrix)
+}
 
 #' Double the numbers that were sent in
 #' @param body The numbers to add
@@ -32,8 +51,8 @@ function(body){
   
 }
 
-#' ANOVA
-#' @param body The numbers to add
+#' Echo
+#' @param body The numbers to echo
 #' @post /echoIt
 #' @json
 function(body){
@@ -46,3 +65,16 @@ function(body){
   
 }
 
+#' ANOVA
+#' @param body The numbers to add
+#' @post /AnovaIt
+#' @json
+function(body){
+  
+  body_matrix <- fromJSON(body)
+  
+  output_matrix <- AnovaIt(body_matrix)
+  
+  toJSON(output_matrix)
+  
+}
